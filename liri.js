e839399b95dbd8de6e7d5dd204b367d18408404c
Nathaniel876 <nathanieltaylor876@gmail.com>
2018-10-06 22:13:52 -0400

    initial commit

--- liri.js
+++ liri.js
@@ -1,16 +1,16 @@
 var request = require('request');
 var moment = require('moment');
-var dotenv =require("dotenv").config();
-var Spotify=require("node-spotify-api");
+var dotenvCon = require("dotenv").config();
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var media = process.argv.slice(3).join(" ");
 
-if (process.argv[2] == "concert-this") {
+if (process.argv[2] === "concert-this") {
     concertThis();
 }
 
-function concertThis(){
+function concertThis() {
     console.log(media)
     var urlquery = "https://rest.bandsintown.com/artists/" + media + "/events?app_id=codingbootcamp";
 
@@ -19,17 +19,17 @@ function concertThis(){
         console.log("Upcoming concerts for " + media + ":")
         var obj = JSON.parse(body);
         for (var set in obj) {
-            var date = moment(obj[set].datetime).format("YYYY/MM/DD");
+            var date = moment(obj[set].datetime).format("MM/DD/YYYY");
             console.log("At " + obj[set].venue.name + " " + obj[set].venue.city + " " + date);
         }
     })
 }
-if (process.argv[2] == "spotify-this-song") {
+if (process.argv[2] === "spotify-this-song") {
     spotifyThisSong();
 }
 
 
-function spotifyThisSong(){
+function spotifyThisSong() {
     console.log(media);
     spotify
         .search({ type: 'track', query: media })
@@ -49,11 +49,11 @@ function spotifyThisSong(){
         });
 }
 
-if (process.argv[2] == "movie-this") {
+if (process.argv[2] === "movie-this") {
     movieThis();
 }
 
-function movieThis(){
+function movieThis() {
     console.log(media)
     var queryUrl = "http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function (error, response, body) {
@@ -67,34 +67,34 @@ function movieThis(){
             console.log("Language: " + movie.Language);
             console.log("Plot: " + movie.Plot);
             console.log("Actors: " + movie.Actors);
-            console.log(movie.Ratings[1].Source+":"+movie.Ratings[1].Value);
+            console.log(movie.Ratings[1].Source + ":" + movie.Ratings[1].Value);
         }
     });
 }
 
 var fs = require("fs");
-fs.readFile("random.txt", "UTF-8", function(error, data) {
+fs.readFile("random.txt", "UTF-8", function (error, data) {
 
     if (error) {
-      return console.log(error);
+        return console.log(error);
     }
-  
+
     var dataList = data.split(",");
-  
+
     console.log(dataList);
-  
+
     media = dataList[1];
-    
-if (dataList[0]=="spotify-this-song"){
-    
-    spotifyThisSong();
-} else if (dataList[0]=="concert-this"){
-  
-    concertThis();
-} else if (dataList[0]=="movie-this"){
-    
-    movieThis();
-} else {
-    console.log("Use one of the commands to get LiRi searching for you!")
-}
-  });
+
+    if (dataList[0] === "spotify-this-song") {
+
+        spotifyThisSong();
+    } else if (dataList[0] === "concert-this") {
+
+        concertThis();
+    } else if (dataList[0] === "movie-this") {
+
+        movieThis();
+    } else {
+        console.log("Use one of the commands to get LiRi searching for you!")
+    }
+});
